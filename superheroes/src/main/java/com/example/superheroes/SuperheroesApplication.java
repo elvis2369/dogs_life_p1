71db0a92332fba6ab4bd76e9524f691645cd5461
Robert <robert.i@hotmail.de>
2023-07-28 11:22:07 +0200

    Added PostConstruct, PreDestroy, Calculator Class
    Signed-off-by: Robert <robert.i@hotmail.de>

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,5 +1,7 @@
 package com.example.superheroes;
 
+import com.example.superheroes.practice.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
 
+	@Autowired
+	Calculator calc;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+
+		Calculator calculator = new Calculator();
+		int result = calculator.addition(3,4);
+
+		System.out.println("Hello World\n"+"Calculator result = "+result);
 	}
 }
