52d30655cd8d9b3081fc1aeebb51e56401b60493
Robert <robert.i@hotmail.de>
2023-07-28 14:55:00 +0200

    Added Query in HeroRepository
    Signed-off-by: Robert <robert.i@hotmail.de>

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -3,6 +3,7 @@ package com.example.superheroes;
 import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
 import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,8 +17,12 @@ public class SuperheroesApplication implements CommandLineRunner {
 	//@Autowired
 	//Calculator calc;
 
+	//@Autowired
+	//HeroRepository heroRepository;
+
 	@Autowired
-	HeroRepository heroRepository;
+	HeroService heroService;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -31,8 +36,13 @@ public class SuperheroesApplication implements CommandLineRunner {
 		//System.out.println("Hello World\n"+"Calculator result = "+result);
 
 
-		List<Hero> heroes = heroRepository.findAll();
-		for (Hero hero: heroes){
+		//List<Hero> heroes = heroRepository.findAll();
+		//for (Hero hero: heroes){
+			//System.out.println(hero);
+	//}
+		List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
+		for (Hero hero : heroes){
 			System.out.println(hero);
-	}
+		}
+
 } }
