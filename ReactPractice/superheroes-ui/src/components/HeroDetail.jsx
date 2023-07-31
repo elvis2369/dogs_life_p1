855e0ef24c43e78265ffa1907a7f14635e72014b
Robert <robert.i@hotmail.de>
2023-07-31 16:37:29 +0200

    Added superhero pics

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import batman from '../images/batman.jpeg';
+//import batman from '../images/1.jpeg';
+import images from '..'
 
-const HeroDetail = () => {
-  return (
-    <div>
-      <img src={batman} alt="Batman" height="250px"/>
-      <p id="blue-alias">Alias: Batman</p>
-      <p className="green-class">Name: Bruce Wayne</p>
-      <button className="green-class">Like</button>   
-    </div>
-  )
-}
+const HeroDetail = (props) => {
+    return (
+      <div>
+        <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>
+        <p id="blue-alias">Alias: {props.info.alias}</p>
+        <p className="green-class">Name: {props.info.name}</p>
+        <button className="green-class">Like</button>   
+      </div>
+    )
+  }
 
 export default HeroDetail
